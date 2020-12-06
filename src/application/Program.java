7b2f1badf6ce3c8e173602b240ac2b681f2a47cf
TCris-ols <teresa.cristols@gmail.com>
2020-12-06 12:02:57 -0300

    Reusing instantation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,7 @@
 package application;
 
+import java.util.Scanner;
+
 import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Seller;
@@ -7,10 +9,14 @@ import model.entities.Seller;
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
 		SellerDao sellerDao = DaoFactory.createSellerDao();
+		System.out.println("====   TEST 1: seller FindById  ====");
 		
-		Seller seller = sellerDao.findById(3);
+		System.out.print("Localizar o vendedor Id: ");
+		int vendId = sc.nextInt();
+		Seller seller = sellerDao.findById(vendId);
 		System.out.println(seller);
 
 	}
